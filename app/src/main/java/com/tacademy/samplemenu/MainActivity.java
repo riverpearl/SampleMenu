fa46c4a7e2f0eb685308a5f85b3b9929f6a4ecb5
River Pearl <riverpearl0115@gmail.com>
2016-07-26 10:41:48 +0900

    Initial commit

---
+++ app/src/main/java/com/tacademy/samplemenu/MainActivity.java
@@ -0,0 +1,63 @@
+package com.tacademy.samplemenu;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_1);
+        SearchView view = (SearchView) MenuItemCompat.getActionView(item);
+        view.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, "query : " + query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.i("MainActivity", "text : " + newText);
+                return false;
+            }
+        });
+        
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_1 :
+            case R.id.menu_itme_2 :
+            case R.id.submenu_item_1 :
+            case R.id.submenu_item_2 :
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_check :
+                item.setChecked(!item.isChecked());
+                return true;
+            case R.id.menu_check_1 :
+            case R.id.menu_check_2 :
+            case R.id.menu_check_3 :
+                item.setChecked(true);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
